f1aa8b3c68beb891829dbc9c14981bb3c318b5cf
shshhan <seunghyeonhan95@gmail.com>
2022-03-31 01:07:29 +0900

    bootstrap, List

---
+++ src/components/List.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-for="(data, i) in blog" :key="i">
+    <h5>{{ data.title }}</h5>
+    <p>{{ data.date }}</p>
+    <p>{{ data.content }}</p>
+    <p>{{ data.number }}</p>
+
+    </div>
+
+  
+</template>
+
+<script>
+export default {
+    name : 'list',
+    props : {
+        blog : Array,
+    },
+}
+</script>
+
+<style>
+
+</style>
